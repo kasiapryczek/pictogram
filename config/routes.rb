7a0dd66430098f91d5413fe31639324ceeadca1c
kasiapryczek <kpryczek@gmail.com>
2017-01-12 17:28:29 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
-    resources :comments do
-    end
+    resources :comments 
   end
   root 'posts#index'
 end
