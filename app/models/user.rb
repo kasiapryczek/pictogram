3becb0fb313897c222571ae94f9229817aef764a
kasiapryczek <kpryczek@gmail.com>
2017-01-13 12:13:20 +0100

    added user profiles, avatar, bio and links

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, styles: { medium: '180x180#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
-    resources :comments 
+    resources :comments
   end
   root 'posts#index'
+
+  get ':name', to: 'profiles#show', as: :profile
+  get ':name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':name/edit', to: 'profiles#update', as: :update_profile
+
 end
